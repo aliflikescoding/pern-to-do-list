8c89d32194eb2bd94ab43f2b9b5f1cc406ca72ee
Alif Wibowo <alif.likes.coding@gmail.com>
2024-11-03 18:55:17 +0700

    Change pool to setup for deployment

--- backend/db/pool.js
+++ backend/db/pool.js
@@ -1,14 +1,10 @@
 import pg from "pg";
 const { Pool } = pg;
-const user = process.env.DB_USER;
-const password = process.env.DB_PASSWORD;
+
+const databaseUrl = process.env.DATABASE_URL;
 
 const pool = new Pool({
-  host: "localhost",
-  user: `${user}`,
-  database: "perntodo",
-  password: `${password}`,
-  port: 5432,
+  connectionString: databaseUrl,
 });
 
 export default pool;
